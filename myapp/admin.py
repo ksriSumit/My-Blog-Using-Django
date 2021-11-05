ecbefb94bbceeffa6d5d18c9deca0e111e19bd6a
ksriSumit <keshari.1@iitj.ac.in>
2021-11-05 13:18:36 +0530

    Created Home Page. Created database model for posts.

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from . models import Features
+#from . models import Features,posts
+from . models import posts
 # Register your models here.
-admin.site.register(Features)
+#admin.site.register(Features)
+admin.site.register(posts)
--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+from django.db.models.aggregates import Max
+import sys
+from datetime import datetime
 # Create your models here.
-class Features(models.Model):
-    name = models.CharField(max_length=100)
-    details = models.CharField(max_length=100)
+
+class posts(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=200000)
+    created_at = models.DateTimeField(default=datetime.now,blank=True)
